27b4c433c27a5e179aea729c001c3a226f9dd80f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 14:26:19 +0200

    exo5p3 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $pas = 1;
+  while($pas <= 15){
+    echo $pas . ' pas, on y est presque <br />';
+    $pas ++;
+  }
+?>
+    </p>
+  </body>
+</html>
